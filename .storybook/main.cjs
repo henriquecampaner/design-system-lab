52f4eaec940280fa20b865756547e79a19983d50
Henrique <henrique.campaner@live.com>
2022-10-13 09:49:34 +0100

    adjust name

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, {configType}) => {
     if(configType === 'PRODUCTION') {
-      config.base = '/lab-design/'
+      config.base = '/design-system-lab/'
     }
 
     return config
